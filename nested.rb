b6e189110e9299ff9c24889df3dd4aac461fe9d6
Shelley Brook <sbrook13@gmail.com>
2020-07-17 17:24:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,16 +59,10 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-:yukihiro_matsumoto => {
-:known_for => "Ruby",
- :languages => ["LISP", "C"]
-}
-# return the entire updated hash
+
 
 	programmer_hash = 
- 		{
-      :grace_hopper => {
+ 		{ :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
       },
@@ -81,13 +75,14 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:yukihiro_matsumoto] = {
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+      }
+programmer_hash
 end
 
 def changing_alan
-  # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-  # return the entire updated hash
-
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -103,13 +98,11 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:alan_kay][:known_for] = "GUI"  
+  programmer_hash
 end
 
 def adding_to_dennis
-  # add "Assembly" to Dennis Ritchie's languages array
-  # return the entire updated hash
-
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -125,5 +118,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages].push("Assembly")  
+  programmer_hash
 end
